1ae105f75ed660ef41c0764217477bcff56c813e
Saurav-Invite2026 <Souravtejpal1998@gmail.com>
2026-07-28 00:58:43 +0530

    Update wedding invitation

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-window.addEventListener("load", function () {
+const envelopeBtn = document.getElementById("envelopeBtn");
+const envelopeScreen = document.getElementById("envelopeScreen");
+
+envelopeBtn.addEventListener("click", function () {
+
+    envelopeScreen.style.opacity = "0";
+    envelopeScreen.style.pointerEvents = "none";
+
     setTimeout(function () {
-        document.getElementById("loader").style.display = "none";
-    }, 1500);
-});
+        envelopeScreen.style.display = "none";
+    }, 500);
 
-const openBtn = document.getElementById("openBtn");
-
-openBtn.addEventListener("click", function () {
-    alert("Welcome to Roshni ❤️ Saurabh Wedding");
 });
